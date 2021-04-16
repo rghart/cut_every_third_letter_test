0b4bcb2337a0a9d758f3253065eac17843ac2c01
Ryan Hart <rhart@lyft.com>
2021-04-16 16:17:59 -0500

    Updating regex to remove non-letters

--- app/helpers.js
+++ app/helpers.js
@@ -1,6 +1,6 @@
 const cutEveryThirdLetterFromString = (string) => {
   return string
-    .replace(/\s+/g, '') //Removing whitespace
+    .replace(/[^a-zA-Z]+/gm, '')
     .split('')
     .filter((_c, i) => (i + 1) % 3 === 0)
     .join('');
